34b5325c52c08f0a1eecbe047162d2d5ae204e94
walidmadad <walid.madad123@gmail.com>
2024-04-27 13:55:02 +0200

    first comit

---
+++ script.js
@@ -0,0 +1,19 @@
+const countLabel = document.getElementById('count');
+const decreaseBtn = document.getElementById('decreaseBtn');
+const increaseBtn = document.getElementById('increaseBtn');
+const resetBtn = document.getElementById('resetBtn');
+
+let count = Number(countLabel.innerHTML);
+
+decreaseBtn.onclick = function(){
+    count--;
+    countLabel.textContent = count;
+}
+increaseBtn.onclick = function(){
+    count++;
+    countLabel.textContent = count;
+}
+resetBtn.onclick = function(){
+    count = 0;
+    countLabel.textContent = count;
+}
